7866d6357e39b86c96caf479a2909b8fb8418839
heecer <xiaoboy00@mail.com>
2026-08-23 23:08:59 +0800

    bugfix: 修复a.js文件的递归求和函数

--- a.js
+++ a.js
@@ -1,5 +1,15 @@
-console.log('a');
+console.log('a文件实现了递归求和');
 
 function a(...args){
   console.log(args)
 }
+
+// 递归求和函数
+function sum(n){
+    if(n === 1){
+        return 1;
+    }
+    return n + sum(n - 1);
+}
+
+console.log(sum(100));
