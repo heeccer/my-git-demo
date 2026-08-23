3003b93c2d9ff46f93e3bf9a047859af7e625268
heecer <37954129+heecer@users.noreply.github.com>
2026-08-24 01:06:27 +0800

    Feature/feat2 (#4)
    * feat: 添加减法函数
    
    * feat: 添加乘法函数
    
    * feat: Update b.js
    
    * feat: 斐波那契的传值更改为30
    
    ---------
    
    Co-authored-by: heeccer <58688991+heeccer@users.noreply.github.com>

--- c.js
+++ c.js
@@ -1,10 +1,33 @@
 console.log("c.js的功能");
 
+// 加法
 function add(a, b) {
     return a + b;
 }
 
 console.log(add(1, 2));
+
+// 减法
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log(subtract(1, 2));
+
+
+// 除法
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(divide(1, 2));
+
+// 乘法
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(1, 2));
 
 // 除法
 function divide(a, b) {
