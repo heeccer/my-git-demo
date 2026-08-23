2236a3df2e87d351c6744d5d651631eb25b3f99a
heecer <xiaoboy00@mail.com>
2026-08-24 00:09:02 +0800

    feat: 添加减法函数

--- c.js
+++ c.js
@@ -1,10 +1,19 @@
 console.log("c.js的功能");
 
+// 加法
 function add(a, b) {
     return a + b;
 }
 
 console.log(add(1, 2));
+
+// 减法
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log(subtract(1, 2));
+
 
 // 除法
 function divide(a, b) {
