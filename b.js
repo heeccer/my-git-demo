47d1c820b8a035c9fe52d0089f110dbbfc4ef27f
heecer <xiaoboy00@mail.com>
2026-08-23 23:11:41 +0800

    bugfix: 修复b.js文件的斐波拉契数列函数

--- b.js
+++ b.js
@@ -1,1 +1,14 @@
-console.log('b');
+console.log('b文件实现了斐波拉契数列');
+
+// 斐波拉契数列
+function fibonacci(n){
+    if(n === 0){
+        return 0;
+    }
+    if(n === 1){
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(10));
